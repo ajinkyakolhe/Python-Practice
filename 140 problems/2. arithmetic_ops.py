4cc500d3885c8736dcd30dd58dc846731a420de5
ajinkyakolhe <ajinkya.kolhe@yahoo.com>
2023-12-07 20:38:09 -0500

    arith ops without test cases

---
+++ 140 problems/2. arithmetic_ops.py
@@ -0,0 +1,5 @@
+# Addition 
+num1 = float(input("Enter the first num: "))
+num2 = float(input("Enter the second num: "))
+result = num1 + num2
+print(f" sum total: {int(num1)} + {int(num2)} = {int(result)} ")
